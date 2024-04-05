fcadd2971516b13440e28932f935f6430bccc422
RaviChauhan <ravic@o365.siprtc.io>
2024-04-05 18:07:28 +0530

    init commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	exchangeName := "my_exchange"
+	queueName := "my_queue"
+	myRoutingKey := "my_routing_key"
+
+	err = ch.ExchangeDeclare(
+		exchangeName, // exchange name
+		"direct",     // exchange type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		queueName, // queue name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,       // queue name
+		myRoutingKey, // routing key
+		exchangeName, // exchange name
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to bind a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		exchangeName, // exchange
+		myRoutingKey, // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+}
